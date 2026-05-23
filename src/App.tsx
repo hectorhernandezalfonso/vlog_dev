5b57c968006a5c8a1a0b82911b83fdf5ce29b8e4
hectorhernandezalfonso <halfonsoh@unal.edu.co>
2026-05-23 17:35:07 -0500

    updated app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import PostDetail from './pages/PostDetail';
 
-const base = import.meta.env.BASE_URL;  
 function App() {
   return (
-    <BrowserRouter basename={base}>
+    <BrowserRouter basename="/vlog_dev">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/post/:slug" element={<PostDetail />} />
