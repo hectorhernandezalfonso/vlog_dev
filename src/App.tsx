03167077099d4a7f884118022cb463c633c2c656
hectorhernandezalfonso <halfonsoh@unal.edu.co>
2026-05-23 17:39:29 -0500

    Switch to HashRouter for GitHub Pages compatibility

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import PostDetail from './pages/PostDetail';
 
 function App() {
   return (
-    <BrowserRouter basename="/vlog_dev">
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/post/:slug" element={<PostDetail />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
